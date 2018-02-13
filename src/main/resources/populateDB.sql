58dea6050f14db6edc471d168536b5d52c56e940
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-02-13 10:38:48 +0300

    Added tables customers and projects_customers

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -1,68 +1,85 @@
 /*Добавление разработчиков*/
 
-# INSERT INTO developers (dev_id, dev_name) VALUES (1,'Kuznecov Ivan');
-# INSERT INTO developers (dev_id, dev_name) VALUES (2,'Fominykh Vladimir');
-# INSERT INTO developers (dev_id, dev_name) VALUES (3,'Glazirin Evgeniy');
-# INSERT INTO developers (dev_id, dev_name) VALUES (4,'Nikiforov Sergey');
-# INSERT INTO developers (dev_id, dev_name) VALUES (5,'Zhdanov Konstantin');
+INSERT INTO developers (dev_id, dev_name) VALUES (1, 'Kuznecov Ivan');
+INSERT INTO developers (dev_id, dev_name) VALUES (2, 'Fominykh Vladimir');
+INSERT INTO developers (dev_id, dev_name) VALUES (3, 'Glazirin Evgeniy');
+INSERT INTO developers (dev_id, dev_name) VALUES (4, 'Nikiforov Sergey');
+INSERT INTO developers (dev_id, dev_name) VALUES (5, 'Zhdanov Konstantin');
 
 /*Добавление навыка разработчика*/
 
-# INSERT INTO skills (sk_id, sk_name) VALUES (1,'Java');
-# INSERT INTO skills (sk_id, sk_name) VALUES (2,'C++');
-# INSERT INTO skills (sk_id, sk_name) VALUES (3,'PHP');
-# INSERT INTO skills (sk_id, sk_name) VALUES (4,'JS');
-# INSERT INTO skills (sk_id, sk_name) VALUES (5,'Excel');
+INSERT INTO skills (sk_id, sk_name) VALUES (1, 'Java');
+INSERT INTO skills (sk_id, sk_name) VALUES (2, 'C++');
+INSERT INTO skills (sk_id, sk_name) VALUES (3, 'PHP');
+INSERT INTO skills (sk_id, sk_name) VALUES (4, 'JS');
+INSERT INTO skills (sk_id, sk_name) VALUES (5, 'Excel');
 
 /*Добавление данных в таблицу навыки и разработчики*/
 
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(1,1,5);
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(2,2,1);
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(3,3,2);
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(4,3,4);
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(5,4,3);
-# INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES(6,5,4);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (1, 1, 5);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (2, 2, 1);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (3, 3, 2);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (4, 3, 4);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (5, 4, 3);
+INSERT INTO skills_developers (sk_dev_id, dev_id, sk_id) VALUES (6, 5, 4);
+
+/*Добавление проектов в таблицу projects*/
+
+INSERT INTO projects (proj_id, proj_name) VALUES (1, 'Database');
+INSERT INTO projects (proj_id, proj_name) VALUES (2, 'Websait');
+INSERT INTO projects (proj_id, proj_name) VALUES (3, 'Blog');
+INSERT INTO projects (proj_id, proj_name) VALUES (4, 'Software "Lotus"');
+
+/*Добавление компаний в таблицу companies*/
+
+INSERT INTO companies (company_id, company_name) VALUES (1, 'HP');
+INSERT INTO companies (company_id, company_name) VALUES (2, 'AK BARS BANK');
+INSERT INTO companies (company_id, company_name) VALUES (3, 'FrutoNyanya');
+INSERT INTO companies (company_id, company_name) VALUES (4, 'OAO "ZPP"');
+INSERT INTO companies (company_id, company_name) VALUES (5, 'TatNeft');
+
+/*Добавление данных в таблицу компании и проекты*/
+
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (1, 2, 1);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (2, 1, 3);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (3, 4, 4);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (4, 3, 1);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (5, 3, 3);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (6, 2, 5);
+INSERT INTO projects_companies (proj_company_id, proj_id, company_id) VALUES (7, 4, 2);
+
+/*Добавление данных о клиенте компании*/
+
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (1, 'Ilya Petrov', 'Moscow, Lomonosova 22-10');
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (2, 'Larisa Zvereva', 'Sankt-Peterburg, Lermontova 2-110');
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (3, 'Ekaterina Solovjeva', 'Samara, Ishpaya 52-5');
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (4, 'Galina Ganina', 'Yoshkar-Ola, Pushkina 16-3');
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (5, 'Aleksandr Korolen', 'Kazan, Dekabristov 1-89');
+INSERT INTO customers (cust_id, cust_name, cust_address) VALUES (6, 'Maksim Petrov', 'Moscow, Zelenaya 24-11');
+
+/*Добавление данных в таблицу клиенты компании и их проекты*/
+
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (1, 1, 1);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (2, 1, 2);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (3, 2, 3);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (4, 2, 6);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (5, 3, 5);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (6, 3, 4);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (7, 4, 2);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (8, 4, 1);
+INSERT INTO projects_customers (proj_cust_id, proj_id, cust_id) VALUES (9, 1, 6);
 
 # ALTER TABLE developers ADD CONSTRAINT un_dev_id_constraint UNIQUE (dev_id);
 # ALTER TABLE skills ADD CONSTRAINT un_sk_id_constraint UNIQUE (sk_id);
 # ALTER TABLE skills_developers ADD CONSTRAINT un_sk_dev_id_constraint UNIQUE (sk_dev_id);
-
+#
 # SELECT * FROM developers JOIN skills_developers USING(dev_id);
 # SELECT * FROM (developers JOIN skills_developers USING(dev_id)) JOIN skills USING (sk_id) WHERE developers.dev_name = 'Glazirin Evgeniy';
 # SELECT dev_name, sk_name FROM (developers JOIN skills_developers USING(dev_id)) JOIN skills USING (sk_id) WHERE developers.dev_id = 3;
 # SELECT * FROM (developers JOIN skills_developers USING(dev_id)) JOIN skills USING (sk_id) WHERE skills.sk_id = 4;
-
-# /*Добавление проектов в таблицу projects*/
 #
-# INSERT INTO projects (proj_id, proj_name) VALUES (1, 'Database');
-# INSERT INTO projects (proj_id, proj_name) VALUES (2, 'Websait');
-# INSERT INTO projects (proj_id, proj_name) VALUES (3, 'Blog');
-# INSERT INTO projects (proj_id, proj_name) VALUES (4, 'Software "Lotus"');
-#
-# /*Добавление компаний в таблицу companies*/
-#
-# INSERT INTO companies(company_id, company_name) VALUES (1, 'HP');
-# INSERT INTO companies(company_id, company_name) VALUES (2, 'AK BARS BANK');
-# INSERT INTO companies(company_id, company_name) VALUES (3, 'FrutoNyanya');
-# INSERT INTO companies(company_id, company_name) VALUES (4, 'OAO "ZPP"');
-# INSERT INTO companies(company_id, company_name) VALUES (5, 'TatNeft');
-#
-# /*Добавление данных в таблицу компании и проекты*/
-#
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (1,2,1);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (2,1,3);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (3,4,4);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (4,3,1);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (5,3,3);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (6,2,5);
-# INSERT INTO projects_companies(proj_company_id, proj_id, company_id) VALUES (7,4,2);
-
 # ALTER TABLE companies ADD CONSTRAINT un_company_id_constraint UNIQUE (company_id);
 # ALTER TABLE projects ADD CONSTRAINT un_proj_id_constraint UNIQUE (proj_id);
 # ALTER TABLE projects_companies ADD CONSTRAINT un_proj_company_id_constraint UNIQUE (proj_company_id);
-
+#
 # SELECT * FROM companies JOIN projects_companies USING(company_id);
-
-
-
-
