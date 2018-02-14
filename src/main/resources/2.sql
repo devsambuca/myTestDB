5a7964d482b4ab6f022b0d2ca16dc4419c360d65
Fominykh <stoyakovi4@gmail.com>
2018-02-14 23:00:59 +0300

    Change 2.sql

--- src/main/resources/2.sql
+++ src/main/resources/2.sql
@@ -1,8 +1,8 @@
 SELECT developers.*,projects.* FROM developers,projects_developers,projects
 WHERE projects.id = projects_developers.proj_id AND developers.id = projects_developers.dev_id;
 
-SELECT projects.name, SUM(salary) as AllSales FROM developers,projects_developers,projects
-WHERE projects.id = projects_developers.proj_id AND developers.id = projects_developers.dev_id GROUP BY projects.id);
+SELECT projects.name, salary as AllSales FROM developers,projects_developers,projects
+WHERE projects.id = projects_developers.proj_id AND developers.id = projects_developers.dev_id GROUP BY projects.name;
 
 
 
