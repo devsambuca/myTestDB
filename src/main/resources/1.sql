64a4fa791661865ea50139e23f85256125f09f08
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-02-15 17:17:45 +0300

    Changed file populateDB.sql

--- src/main/resources/1.sql
+++ src/main/resources/1.sql
@@ -8,6 +8,11 @@ UPDATE developers SET salary = 50000 WHERE id = 1;
 UPDATE developers SET salary = 150000 WHERE id = 2;
 UPDATE developers SET salary = 70000 WHERE id = 3;
 UPDATE developers SET salary = 80000 WHERE id = 4;
-UPDATE developers SET salary = 125000 WHERE id = 5;
+UPDATE developers SET salary = 100000 WHERE id = 5;
+UPDATE developers SET salary = 125000 WHERE id = 6;
+UPDATE developers SET salary = 85000 WHERE id = 7;
+UPDATE developers SET salary = 135000 WHERE id = 8;
+UPDATE developers SET salary = 115000 WHERE id = 9;
+
 
 
